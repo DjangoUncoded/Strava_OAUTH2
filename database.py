3e1c402c78bb6a86dade2d2801339ef21b14da0d
unknown <dhruvparlekar@gmail.com>
2025-07-30 19:00:16 +0530

    changes to database

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
-
+import os
 from dotenv import load_dotenv
 load_dotenv()
-import os
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-URL=os.getenv('DATABASE_URL')
-engine=create_engine(URL)
-Session_Local=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("TEST_DATABASE_URL")
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///:memory:"  # fallback
+
+engine = create_engine(DATABASE_URL)
+Session_Local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
